5d70868d67695253a5218e1567366b827bac6e17
jorenvo <joren@jvo.sh>
2024-02-18 18:18:11 +0000

    [SPA] bash is hard

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [ !d /workspaces/enterprise-spaces ]; then
+if [ ! -d /workspaces/enterprise-spaces ]; then
     git clone https://github.com/jorenvo/enterprise-spaces.git /workspaces/enterprise-spaces
 fi
